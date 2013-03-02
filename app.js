6671efa733535329b566d5289f19130c4aa63d51
Kelvin Pompey <kelvin.pompey@gmail.com>
2013-03-02 17:25:10 -0500

    added whatwedo page

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/whoweare', routes.whoweare);
 app.get('/ourwork', routes.ourwork);
+app.get('/whatwedo', routes.whatwedo);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ var _ = require('underscore');
 var links = {
     
     'whoweare': {path: '/whoweare', active: false, content: 'Who we are'},
+    'whatwedo': {path: '/whatwedo', active: false, content: 'What we do'},
     'ourwork' : {path: '/ourwork', active: false, content: 'Our Work'},
     'contact' : {path: '/contactus.html', active: false, content: 'Contact'} 
 };
@@ -19,6 +20,11 @@ exports.index = function(req, res){
     console.log(links);
 
     var home = ejs.render(fs.readFileSync("views/home.ejs", "utf-8"))
+    links['whoweare'].active = false; 
+    links['whatwedo'].active = false; 
+    links['contact'].active = false;       
+   
+    links['ourwork'].active = false;     
     
     res.render('layout.ejs', { title: 'Express', 
                                 partial : home,
@@ -32,7 +38,7 @@ exports.ourwork = function(req, res){
 
     var home = ejs.render(fs.readFileSync("views/ourwork.ejs", "utf-8"))
     links['whoweare'].active = false; 
-    links['ourwork'].active = false; 
+    links['whatwedo'].active = false; 
     links['contact'].active = false;       
    
     links['ourwork'].active = true; 
@@ -48,7 +54,7 @@ exports.whoweare = function(req, res){
 
     var home = ejs.render(fs.readFileSync("views/whoweare.ejs", "utf-8"))
     
-    links['whoweare'].active = false; 
+    links['whatwedo'].active = false; 
     links['ourwork'].active = false; 
     links['contact'].active = false;      
     links['whoweare'].active = true; 
@@ -59,3 +65,22 @@ exports.whoweare = function(req, res){
                                 stylesheets: ['whoweare.css']});
   
 };
+
+exports.whatwedo = function(req, res){
+
+    var home = ejs.render(fs.readFileSync("views/whatwedo.ejs", "utf-8"))
+    
+    links['whoweare'].active = false; 
+    links['ourwork'].active = false; 
+    links['contact'].active = false;      
+    links['whatwedo'].active = true; 
+    res.render('layout.ejs', { title: 'Express', 
+                                partial : home,
+                                links: links,
+                                _ : _,
+                                stylesheets: ['whoweare.css']});
+  
+};
+
+
+
