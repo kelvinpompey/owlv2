56fb7914b3ec66e1767c08b177c66197ba12ed14
Kelvin Pompey <kelvin.pompey@gmail.com>
2013-03-02 17:15:16 -0500

    highlight active menu

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,25 @@
 
 var ejs = require('ejs');
 var fs = require('fs');
+var _ = require('underscore'); 
+
+var links = {
+    
+    'whoweare': {path: '/whoweare', active: false, content: 'Who we are'},
+    'ourwork' : {path: '/ourwork', active: false, content: 'Our Work'},
+    'contact' : {path: '/contactus.html', active: false, content: 'Contact'} 
+};
 
 exports.index = function(req, res){
+    
+    console.log(links);
 
     var home = ejs.render(fs.readFileSync("views/home.ejs", "utf-8"))
     
     res.render('layout.ejs', { title: 'Express', 
                                 partial : home,
+                                links: links,
+                                _ : _,
                                 stylesheets: []});
   
 };
@@ -19,9 +31,15 @@ exports.index = function(req, res){
 exports.ourwork = function(req, res){
 
     var home = ejs.render(fs.readFileSync("views/ourwork.ejs", "utf-8"))
-    
+    links['whoweare'].active = false; 
+    links['ourwork'].active = false; 
+    links['contact'].active = false;       
+   
+    links['ourwork'].active = true; 
     res.render('layout.ejs', { title: 'Express', 
                                 partial : home, 
+                                links: links,
+                                _ : _,
                                 stylesheets: ['ourwork.css'] });
   
 };
@@ -30,8 +48,14 @@ exports.whoweare = function(req, res){
 
     var home = ejs.render(fs.readFileSync("views/whoweare.ejs", "utf-8"))
     
+    links['whoweare'].active = false; 
+    links['ourwork'].active = false; 
+    links['contact'].active = false;      
+    links['whoweare'].active = true; 
     res.render('layout.ejs', { title: 'Express', 
                                 partial : home,
+                                links: links,
+                                _ : _,
                                 stylesheets: ['whoweare.css']});
   
 };
